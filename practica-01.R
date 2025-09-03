4a43f828fc993b5ae4df96270ba3a4602ba15dce
unknown <joelvillacorta04@gmail.com>
2025-09-03 08:55:08 -0500

    updt

---
+++ practica-01.R
@@ -0,0 +1,26 @@
+# conceto basicos de R
+
+# O = 
+# y = &
+# no = !
+
+# asignar alt + -
+
+x <- "Hola"
+y <- "UNTRM"
+
+# R es un lenguaje que trabaja con funciones 
+
+# f(x, y) = 2xy + x
+
+
+# Ejemplo
+
+notas <- c(12, 15, 13, 17)
+notas
+md <- mean(notas)
+nf <- md + 2
+nf
+
+# pipe
+
